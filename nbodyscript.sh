23f63861f956f6a9b93f71b8f605786853667641
David <dsaldiva@uncc.edu>
2025-02-18 14:52:01 -0500

    script to benchmark in centarus using slurm

---
+++ nbodyscript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=nbody_simulation
+#SBATCH --partition=Centaurus
+#SBATCH --time=01:00:00
+#SBATCH --mem=32G
+
+TEST_CASES=(
+    "100 1 10000 100"       
+    "1000 1 10000 100"      
+    "2 200 5000000 1000"    
+)
+
+mkdir -p logs
+
+for test in "${TEST_CASES[@]}"; do    
+    PARAMS=($test)
+    NUM_PARTICLES=${PARAMS[0]}
+    DT=${PARAMS[1]}
+    NUM_ITER=${PARAMS[2]}
+
+    LOG_FILE="logs/output_${NUM_PARTICLES}p_${DT}dt_${NUM_ITER}steps.tsv"
+    
+    ./nbody $test > "$LOG_FILE"
+done
+
